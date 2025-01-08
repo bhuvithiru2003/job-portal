fca6c838fb21a57ac6d22dff0b26fc46975f7652
bhuvithiru2003 <thiruppathithiru06@gmail.com>
2025-01-08 12:55:21 +0530

    Almost Done

--- src/main/java/com/jsp/job_portal/repository/JobSeekerRepository.java
+++ src/main/java/com/jsp/job_portal/repository/JobSeekerRepository.java
@@ -1,9 +1,14 @@
 package com.jsp.job_portal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsp.job_portal.dto.JobApplication;
 import com.jsp.job_portal.dto.JobSeeker;
+
 public interface JobSeekerRepository extends JpaRepository<JobSeeker, Integer> {
-  
+
 	boolean existsByEmail(String email);
 
 	boolean existsByMobile(Long mobile);
@@ -12,5 +17,6 @@ public interface JobSeekerRepository extends JpaRepository<JobSeeker, Integer> {
 
 	JobSeeker findByEmail(String email);
 
-}
+    List<JobSeeker> findByJobApplicationsIn(List<JobApplication> applicaitons);
 
+}
